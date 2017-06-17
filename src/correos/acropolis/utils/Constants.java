12f2cf6c9ccc4247a8ef95310342b424daa5e115
Mauricio Ballesteros <mballes95@gmail.com>
2017-06-17 19:41:49 -0400

    Agregando Archivo de Constantes

---
+++ src/correos/acropolis/utils/Constants.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correos.acropolis.utils;
+
+/**
+ *
+ * @author mauriballes
+ */
+public class Constants {
+
+    // Mail Server Vars
+    public static final String MAIL_SERVER_HOST = "mail.ficct.uagrm.edu.bo";
+    public static final String MAIL_USER = "grupo01sc";
+    public static final String MAIL_PASSWORD = "grupo01grupo01";
+    public static final String MAIL_USERMAIL = "grupo01sc@ficct.uagrm.edu.bo";
+}
