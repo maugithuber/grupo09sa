e187928cbedc4cc48ebda7744121c05c54075bb2
Mauricio Ballesteros <mballes95@gmail.com>
2017-06-23 08:13:08 -0400

    Agregando los Helpers!

--- src/correos/acropolis/utils/Helper.java
+++ src/correos/acropolis/utils/Helper.java
@@ -10,8 +10,44 @@ package correos.acropolis.utils;
  * @author mauriballes
  */
 public class Helper {
+
     public static final String HELP_GLOBAL = "Bienvenido!!!";
     public static final String HELP_OBTENERALUMNOS = "Obtener Alumnos!!!";
     public static final String HELP_REGISTRARALUMNO = "Registrar Alumno!!!";
     public static final String HELP_MODIFICARALUMNO = "Modificar Alumno!!!";
+    public static final String HELP_OBTENERPROFESORES = "Obtener Profesores!!!";
+    public static final String HELP_REGISTRARPROFESOR = "Registrar Profesor!!!";
+    public static final String HELP_MODIFICARPROFESOR = "Modificar Profesor!!!";
+    public static final String HELP_OBTENERINSCRIPCIONES = "Obtener Inscripciones!!!";
+    public static final String HELP_OBTENERDETALLEINSCRIPCION = "Obtener Detalle de Inscripciones!!!";
+    public static final String HELP_OBTENERCURSOSHABILITADOS = "Obtener Cursos Habilitados!!!";
+    public static final String HELP_REGISTRARINSCRIPCION = "Registrar Inscripcion!!!";
+    public static final String HELP_MODIFICARINSCRIPCION = "Modificar Inscripcion!!!";
+    public static final String HELP_ADICIONARGRUPOS = "Adicionar Grupos!!!";
+    public static final String HELP_RETIRARGRUPOS = "Retirar Grupos!!!";
+    public static final String HELP_OBTENERCURSOS = "Obtener Cursos!!!";
+    public static final String HELP_OBTENERPREREQUISITOS = "Obtener Prerequisitos!!!";
+    public static final String HELP_REGISTRARCURSO = "Registrar Curso!!!";
+    public static final String HELP_MODIFICARCURSO = "Modificar Curso!!!";
+    public static final String HELP_OBTENERGRUPOS = "Obtener Grupos!!!";
+    public static final String HELP_OBTENERHORARIOS = "Obtener Horarios!!!";
+    public static final String HELP_REGISTRARGRUPO = "Registrar Grupo!!!";
+    public static final String HELP_MODIFICARGRUPO = "Modificar Grupo!!!";
+    public static final String HELP_ASIGNARGRUPO = "Asignar Grupo!!!";
+    public static final String HELP_REGISTRARHORARIO = "Registrar Horario!!!";
+    public static final String HELP_MODIFICARHORARIO = "Modificar Horario111";
+    public static final String HELP_ELIMINARHORARIO = "Eliminar Horario!!!";
+    public static final String HELP_OBTENERAULAS = "Obtener Aulas!!!";
+    public static final String HELP_REGISTRARAULA = "Registrar Aula!!!";
+    public static final String HELP_MODIFICARAULA = "Modificar Aula!!!";
+    public static final String HELP_OBTENERKARDEXS = "Obtener Kardexs!!!";
+    public static final String HELP_OBTENERASISTENCIAS = "Obtener Asistencias!!!";
+    public static final String HELP_REGISTRARASISTENCIAS = "Registrar Asistencias!!!";
+    public static final String HELP_MODIFICARASISTENCIAS = "Modificar Asistencias!!!";
+    public static final String HELP_REGISTRARNOTA = "Registrar Nota!!!";
+    public static final String HELP_REPORTEHISTORICO = "Reporte Historico!!!";
+    public static final String HELP_REPORTEALUMNOSINSCRITOS = "Reporte Alumnos Inscritos!!!";
+    public static final String HELP_REPORTEOFERTACURSOS = "Reporte Oferta de Cursos!!!";
+    public static final String HELP_AGREGARPREREQUISITO = "Agregar Prerequisito!!!";
+    public static final String HELP_QUITARPREREQUISITO = "Quitar Prerequisito!!!";
 }
