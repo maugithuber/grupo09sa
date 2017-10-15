fe4b22b22a91f3bff6d1712f2504530a8cc7fac3
paul <p>
2017-10-15 18:12:15 -0400

    Correciones sobre detalle

--- src/correos/tiendaonline/software/Datos/Detalle.java
+++ src/correos/tiendaonline/software/Datos/Detalle.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -16,9 +17,10 @@ import javax.swing.table.DefaultTableModel;
  * @author Mauricio
  */
 public class Detalle {
-   int id,id_orden,id_producto,cantidad;
-   float subtotal;
-   Conexion m_Conexion;
+
+    int id, id_orden, id_producto, cantidad;
+    float subtotal;
+    Conexion m_Conexion;
 
     public Detalle() {
         m_Conexion = Conexion.getInstancia();
@@ -31,12 +33,20 @@ public class Detalle {
         this.cantidad = cantidad;
         this.subtotal = subtotal;
     }
-    
+
+    public void setDetalle(int id_orden, int id_producto, int cantidad, float subtotal) {
+
+        this.id_orden = id_orden;
+        this.id_producto = id_producto;
+        this.cantidad = cantidad;
+        this.subtotal = subtotal;
+    }
+
     public DefaultTableModel getDetalle(int id) {
         // Tabla para mostrar lo obtenido de la consulta
         DefaultTableModel detalle = new DefaultTableModel();
         detalle.setColumnIdentifiers(new Object[]{
-              "id", "id_orden", "id_producto","cantidad","subtotal"
+            "id", "id_orden", "id_producto", "cantidad", "subtotal"
         });
 
         // Abro y obtengo la conexion
@@ -47,11 +57,11 @@ public class Detalle {
         String sql = "SELECT\n"
                 + "detalle.id,\n"
                 + "detalle.id_orden,\n"
-                + "detalle.id_producto\n"
-                + "detalle.cantidad\n"
+                + "detalle.id_producto,\n"
+                + "detalle.cantidad,\n"
                 + "detalle.subtotal\n"
-                + "FROM orden\n"
-                + "WHERE orden.id=?";
+                + "FROM detalle\n"
+                + "WHERE detalle.id=?";
         // Los simbolos de interrogacion son para mandar parametros 
         // a la consulta al momento de ejecutalas
 
@@ -72,22 +82,58 @@ public class Detalle {
                     rs.getInt("id_orden"),
                     rs.getInt("id_producto"),
                     rs.getInt("cantidad"),
-                    rs.getFloat("subtotal"),
-                });
+                    rs.getFloat("subtotal"),});
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
         return detalle;
     }
-     
 
+    public int registrarDetalle() {
+        // Abro y obtengo la conexion
+        this.m_Conexion.abrirConexion();
+        Connection con = this.m_Conexion.getConexion();
+
+        // Preparo la consulta
+        String sql = "INSERT INTO detalle(\n"
+                + "id_orden,id_producto,cantidad,subtotal)\n"
+                + "VALUES(?,?,?,?)";
+
+        try {
+            // La ejecuto
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            // El segundo parametro de usa cuando se tienen tablas que generan llaves primarias
+            // es bueno cuando nuestra bd tiene las primarias autoincrementables
+
+            ps.setInt(1, this.id_orden);
+            ps.setInt(2, this.id_producto);
+            ps.setInt(3, this.cantidad);
+            ps.setFloat(4, this.subtotal);
+
+            int rows = ps.executeUpdate();
+
+            // Cierro Conexion
+            this.m_Conexion.cerrarConexion();
+
+            // Obtengo el id generado pra devolverlo
+            if (rows != 0) {
+                ResultSet generateKeys = ps.getGeneratedKeys();
+                if (generateKeys.next()) {
+                    return generateKeys.getInt(1);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return 0;
+    }
 
     public DefaultTableModel getDetalles() {
         // Tabla para mostrar lo obtenido de la consulta
         DefaultTableModel detalles = new DefaultTableModel();
         detalles.setColumnIdentifiers(new Object[]{
-        "id", "id_orden", "id_producto","cantidad","subtotal"
+            "id", "id_orden", "id_producto", "cantidad", "subtotal"
         });
 
         // Abro y obtengo la conexion
@@ -97,8 +143,8 @@ public class Detalle {
         String sql = "SELECT\n"
                 + "detalle.id,\n"
                 + "detalle.id_orden,\n"
-                + "detalle.id_producto\n"
-                + "detalle.cantidad\n"
+                + "detalle.id_producto,\n"
+                + "detalle.cantidad,\n"
                 + "detalle.subtotal\n"
                 + "FROM orden\n";
 
@@ -118,16 +164,12 @@ public class Detalle {
                     rs.getInt("id_orden"),
                     rs.getInt("id_producto"),
                     rs.getInt("cantidad"),
-                    rs.getFloat("subtotal"),
-                });
+                    rs.getFloat("subtotal"),});
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
         return detalles;
     }
-    
-   
-   
-   
+
 }
